1cf50e9ddb0421b9bb0115379a4ac5c91f36627e
Luiz Antonio <poke.tunico@gmail.com>
2025-12-07 22:52:17 -0300

    Update 04b_Intermittent_Demand_Models_Forecast.R

---
+++ scripts/04b_Intermittent_Demand_Models_Forecast.R
@@ -0,0 +1,16 @@
+# 04b - PREVISÃO: MODELOS ESPECIALIZADOS EM DEMANDA INTERMITENTE ####
+#
+# Autor: LUIZ ANTONIO DOS SANTOS DIAS REZENDE
+# Descrição: Implementação de métodos especializados para demanda intermitente
+#            com otimização de hiperparâmetros (Família 3)
+# Data: 2025-12-05
+# Versão: 1.0.0
+
+# Família 3: Métodos Especializados
+#   - Croston Clássico
+#   - SBA (Syntetos-Boylan Approximation)
+#   - TSB (Teunter-Syntetos-Babai)
+#
+# NOTA: Estes métodos são especificamente desenvolvidos para lidar com
+#       demanda intermitente e errática, superando limitações dos métodos
+#       tradicionais em séries com alta proporção de zeros.
